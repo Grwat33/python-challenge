3d33af5cd340398e780d45418e06e5a6ad6b261e
Grwat33 <Grwat33@gmail.com>
2020-09-19 14:15:43 -0400

    new file: analysis/analysis.txt

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -57,3 +57,17 @@ with open(csvpath) as csvfile:
     print('------------------')
     print(f'Winner: {winner}')
     print('------------------')
+
+with open(f'analysis/analysis.txt', 'w') as textfile:
+    textfile.write("                  \n")
+    textfile.write("Election Results\n")
+    textfile.write("------------------\n")
+    textfile.write(f"Total Votes: {countofvotes}\n")
+    textfile.write('------------------\n')
+    textfile.write(f'{noduplicates[0]}: {formattedkhan}% ({khancount})\n')
+    textfile.write(f'{noduplicates[1]}: {formattedcorrey}% ({correycount})\n')
+    textfile.write(f'{noduplicates[2]}: {formattedli}% ({licount})\n')
+    textfile.write(f'{noduplicates[3]}: {formattedotooley}% ({otooleycount})\n')
+    textfile.write('------------------\n')
+    textfile.write(f'Winner: {winner}\n')
+    textfile.write('------------------\n')
